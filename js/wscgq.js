1c4f353f65aa642e36a216d60973ac96c447eb83
xcpab <mx513546@gmail.com>
2025-10-08 11:52:35 +0800

    Update wscgq.js

--- js/wscgq.js
+++ js/wscgq.js
@@ -1,37 +1,15 @@
-const _0x48d8 = ['apply', 'constructor', 'return\x20/\x22\x20+\x20this\x20+\x20\x22/', 'split', 'forEach', 'User-Agent:\x20okhttp/3.11.0', 'Content-Type:\x20application/json', 'Content-Type:\x20application/x-www-form-urlencoded', 'sign', 'get', 'data', 'url', 'success', 'body', 'mediaAsset', 'id'];
+const _0x48d8 = ['apply', 'constructor', 'return\x20/\x22\x20+\x20this\x20+\x20\x22/', 'split', 'forEach', 'User-Agent:\x20okhttp/3.11.0', 'Content-Type:\x20application/json', 'Content-Type:\x20application/x-www-form-urlencoded', 'sign', 'get', 'data', 'url', 'success', 'body', 'mediaAsset', 'id', 'token', 'POST', 'setCache', 'getCache', 'opensslEncrypt'];
 const _0x1299 = function(_0x48d880, _0x12994c) {
     _0x48d880 = _0x48d880 - 0x0;
     let _0x6114c8 = _0x48d8[_0x48d880];
     return _0x6114c8;
 };
-const _0x31d9bf = function() {
-    let _0xe81c55 = !![];
-    return function(_0x154862, _0x3180e9) {
-        const _0x4a275d = _0xe81c55 ? function() {
-            if (_0x3180e9) {
-                const _0x1d3dbf = _0x3180e9[_0x1299('0x0')](_0x154862, arguments);
-                _0x3180e9 = null;
-                return _0x1d3dbf;
-            }
-        } : function() {};
-        _0xe81c55 = ![];
-        return _0x4a275d;
-    };
-}();
-const _0x500225 = _0x31d9bf(this, function() {
-    const _0x53bb5f = function() {
-        const _0x399e5f = _0x53bb5f[_0x1299('0x1')](_0x1299('0x2'))()['compile']('^([^\x20]+(\x20+[^\x20]+)+)+[^\x20]}');
-        return !_0x399e5f['test'](_0x500225);
-    };
-    return _0x53bb5f();
-});
-_0x500225();
 
 function parseItem(_0x53c2c1) {
     const _0x1318a4 = {};
     if (!_0x53c2c1) return _0x1318a4;
-    _0x53c2c1[_0x1299('0x3')]('&')[_0x1299('0x4')](_0x296f3b => {
-        const [_0x52cd66, _0x1c0c7e] = _0x296f3b[_0x1299('0x3')]('=');
+    _0x53c2c1['split']('&')['forEach'](_0x296f3b => {
+        const [_0x52cd66, _0x1c0c7e] = _0x296f3b['split']('=');
         if (_0x52cd66) _0x1318a4[_0x52cd66] = _0x1c0c7e;
     });
     return _0x1318a4;
@@ -73,11 +51,11 @@ function main(_0x3e1db6) {
             _0x143719 += "#EXTINF:-1," + _0x5d0f14 + "\n";
             _0x143719 += _0x92a547 + "?id=" + _0x224e36 + "\n";
         }
-        return JSON.stringify({ 'm3u8': _0x143719 });
+        return _0x143719;
     }
 
     // 生成签名函数
-    function _0x183a5e(_0x2b8f9a, _0x3a7e8d, _0x5c8e12, _0x1f7c6e) {
+    function makeSign(_0x2b8f9a, _0x3a7e8d, _0x5c8e12, _0x1f7c6e) {
         const _0x4d3e80 = { url: _0x2b8f9a, params: _0x3a7e8d, time: _0x5c8e12 };
         const _0x23e16c = JSON.stringify(_0x4d3e80);
         // 使用AES-256-ECB加密
@@ -93,24 +71,35 @@ function main(_0x3e1db6) {
     let _0x23e16c = mytv.getCache('visitor_token');
     if (!_0x23e16c) {
         const _0x4d3e80 = Date.now();
-        const _0x143719 = _0x183a5e(_0x92a5471, '', _0x4d3e80, _0x37cbd8);
-        const _0x224e36 = [_0x1299('0x6'), _0x1299('0x8') + ': ' + _0x143719];
+        const _0x143719 = makeSign(_0x92a5471, '', _0x4d3e80, _0x37cbd8);
+        const _0x224e36 = {
+            'Content-Type': 'application/json',
+            'headers': '1.1.3',
+            'sign': _0x143719
+        };
         
-        const _0x5d0f14 = mytv[_0x1299('0x9')](_0x92a5471, _0x224e36, 'POST', '');
-        if (!_0x5d0f14) return JSON.stringify({ 'url': '' });
+        const _0x5d0f14 = mytv.request(_0x92a5471, {
+            method: 'POST',
+            headers: _0x224e36,
+            body: ''
+        });
+        
+        if (!_0x5d0f14 || !_0x5d0f14.body) {
+            return '';
+        }
         
         let _0x183a5e;
         try {
-            _0x183a5e = JSON.parse(_0x5d0f14);
+            _0x183a5e = JSON.parse(_0x5d0f14.body);
         } catch (_0x2b8f9a) {
-            return JSON.stringify({ 'url': '' });
+            return '';
         }
         
-        if (!_0x183a5e[_0x1299('0xb')] || !_0x183a5e[_0x1299('0xa')] || !_0x183a5e[_0x1299('0xa')].token) {
-            return JSON.stringify({ 'url': '' });
+        if (!_0x183a5e.success || !_0x183a5e.data || !_0x183a5e.data.token) {
+            return '';
         }
         
-        _0x23e16c = _0x183a5e[_0x1299('0xa')].token;
+        _0x23e16c = _0x183a5e.data.token;
         mytv.setCache('visitor_token', _0x23e16c, 86400000);
     }
 
@@ -132,20 +121,31 @@ function main(_0x3e1db6) {
         const _0x4a275d = "1.1.4";
         const _0x53bb5f = "cityId=" + _0x1c0c7e + "&columnId=" + _0x52cd66 + "&provinceId=" + _0x3180e9 + "&token=" + encodeURIComponent(_0x23e16c) + "&version=" + _0x4a275d;
         const _0x399e5f = Date.now();
-        const _0x6114c8 = _0x183a5e(_0x92a5472, _0x53bb5f, _0x399e5f, _0x37cbd8);
-        const _0xe81c55 = [_0x1299('0x5'), _0x1299('0x7'), _0x1299('0x8') + ': ' + _0x6114c8];
+        const _0x6114c8 = makeSign(_0x92a5472, _0x53bb5f, _0x399e5f, _0x37cbd8);
+        const _0xe81c55 = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'okhttp/3.11.0',
+            'sign': _0x6114c8
+        };
         
-        const _0x154862 = mytv[_0x1299('0x9')](_0x92a5472, _0xe81c55, 'POST', _0x53bb5f);
-        if (!_0x154862) return JSON.stringify({ 'url': '' });
+        const _0x154862 = mytv.request(_0x92a5472, {
+            method: 'POST',
+            headers: _0xe81c55,
+            body: _0x53bb5f
+        });
         
-        try {
-            _0x1318a4 = JSON.parse(_0x154862);
-        } catch (_0x1d3dbf) {
-            return JSON.stringify({ 'url': '' });
+        if (!_0x154862 || !_0x154862.body) {
+            return '';
         }
         
-        if (!_0x1318a4[_0x1299('0xb')]) {
-            return JSON.stringify({ 'url': '' });
+        try {
+            _0x1318a4 = JSON.parse(_0x154862.body);
+        } catch (_0x1d3dbf) {
+            return '';
+        }
+        
+        if (!_0x1318a4.success) {
+            return '';
         }
         
         mytv.setCache('column_all_list_33', JSON.stringify(_0x1318a4), 600000);
@@ -155,11 +155,11 @@ function main(_0x3e1db6) {
     const _0x1d3dbf = _0x21cb05[_0xa080c8] || 91417;
     let _0x2b8f9a = null;
    
-    if (_0x1318a4 && _0x1318a4[_0x1299('0xa')] && Array.isArray(_0x1318a4[_0x1299('0xa')])) {
-        for (let _0x3a7e8d = 0x0; _0x3a7e8d < _0x1318a4[_0x1299('0xa')].length; _0x3a7e8d++) {
-            const _0x5c8e12 = _0x1318a4[_0x1299('0xa')][_0x3a7e8d];
-            if (_0x5c8e12[_0x1299('0xc')] && _0x5c8e12[_0x1299('0xc')][_0x1299('0xd')] === _0x1d3dbf) {
-                _0x2b8f9a = _0x5c8e12[_0x1299('0xc')][_0x1299('0xe')];
+    if (_0x1318a4 && _0x1318a4.data && Array.isArray(_0x1318a4.data)) {
+        for (let _0x3a7e8d = 0; _0x3a7e8d < _0x1318a4.data.length; _0x3a7e8d++) {
+            const _0x5c8e12 = _0x1318a4.data[_0x3a7e8d];
+            if (_0x5c8e12.mediaAsset && _0x5c8e12.mediaAsset.id === _0x1d3dbf) {
+                _0x2b8f9a = _0x5c8e12.mediaAsset.url;
                 break;
             }
         }
@@ -168,13 +168,13 @@ function main(_0x3e1db6) {
     // 返回播放地址
     if (_0x2b8f9a) {
         return JSON.stringify({
-            'url': _0x2b8f9a,
-            'headers': {
+            url: _0x2b8f9a,
+            headers: {
                 'User-Agent': 'aliplayer',
                 'Referer': 'https://api.chinaaudiovisual.cn/'
             }
         });
     }
    
-    return JSON.stringify({ 'url': '' });
+    return '';
 }
